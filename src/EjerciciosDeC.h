e041d07a949e3cc27459f9f18e305ae5906534de
MateoSilva626 <msilvatrinidad@frba.utn.edu.ar>
2022-04-18 10:30:23 -0300

    subo el repo

---
+++ src/EjerciciosDeC.h
@@ -0,0 +1,58 @@
+#ifndef EJERCICIOSDEC_H_
+#define EJERCICIOSDEC_H_
+
+
+
+//Ejercicio 1 (Básico de C)
+
+
+/*
+*   Retorna un String nuevo que es la concatenación
+*   de los dos Strings pasados por parámetro
+*   Ejemplo:
+*   char* nombre = "Ritchie";
+*   char* saludo = string_concat("Hola ", nombre);
+*   =>
+*   saludo = "Hola Ritchie"
+*/
+char* string_concat(const char*, const char*);
+
+/*
+*   Asigna en el tercer parámetro, la concatenación
+*   de los primeros dos Strings
+*   Ejemplo:
+*   char* nombre = "Ritchie";
+*   char* saludo;
+*   string_concat("Hola ", nombre, &saludo);
+*   =>
+*   saludo = "Hola Ritchie"
+*/
+void string_concat_dinamyc(const char*, const char*, char**);
+
+/*
+*   Separa el mail en un usuario y un dominio.
+*   Ejemplo:
+*   char* mail = "ritchie@ansic.com.ar";
+*   char* user;
+*   char* dominio;
+*   mail_split(mail, &user, &dominio);
+*   =>
+*   user = "ritchie"
+*   dominio = "ansic.com.ar"
+*/
+void mail_split(const char* mail, char** user, char** dominio);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* EJERCICIOSDEC_H_ */
